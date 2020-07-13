8b3bb96534e244dce35f2d15d0d5e4c192b0c63c
rustiever <sharanzoff@gmail.com>
2020-07-13 22:59:18 +0530

    removed bugs in flutter_tts implementation

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -22,7 +22,7 @@ class _HomeState extends State<Home> {
   Kural kural;
   List<Kurals> kurals;
   bool shuffle = false, isLoading = true, isTtsPlaying = false;
-  int limit = 5;
+  int limit = 10;
 
   bool moreAvail = true, isMoreLoading = false;
 
@@ -65,6 +65,7 @@ class _HomeState extends State<Home> {
       setState(() {
         print("Playing");
         kural.kurals[index].isPlaying = true;
+        isTtsPlaying = true;
         ttsState = TtsState.playing;
       });
     });
@@ -74,6 +75,7 @@ class _HomeState extends State<Home> {
         print("Complete");
         ttsState = TtsState.stopped;
         kural.kurals[index].isPlaying = false;
+        isTtsPlaying = false;
       });
     });
 
@@ -81,6 +83,7 @@ class _HomeState extends State<Home> {
       setState(() {
         print("Cancel");
         kural.kurals[index].isPlaying = false;
+        isTtsPlaying = false;
         ttsState = TtsState.stopped;
       });
     });
@@ -90,6 +93,7 @@ class _HomeState extends State<Home> {
         setState(() {
           print("Paused");
           kural.kurals[index].isPlaying = false;
+          isTtsPlaying = false;
           ttsState = TtsState.paused;
         });
       });
@@ -98,6 +102,7 @@ class _HomeState extends State<Home> {
         setState(() {
           print("Continued");
           kural.kurals[index].isPlaying = true;
+          isTtsPlaying = true;
           ttsState = TtsState.continued;
         });
       });
@@ -107,6 +112,7 @@ class _HomeState extends State<Home> {
       setState(() {
         print("error: $msg");
         kural.kurals[index].isPlaying = false;
+        isTtsPlaying = false;
         ttsState = TtsState.stopped;
       });
     });
@@ -253,9 +259,9 @@ class _HomeState extends State<Home> {
                       itemBuilder: (BuildContext context, int index) {
                         // print(kural.kurals[1].isPlaying.toString() + '   kk');
                         // print(sam.toString() + ' . sss');
-                        kurals = kural.kurals;
+                        // kurals = kural.kurals;
                         if (shuffle) {
-                          kurals.shuffle();
+                          kural.kurals.shuffle();
                           shuffle = false;
                         } //else
                         return Card(
@@ -269,10 +275,6 @@ class _HomeState extends State<Home> {
                           ),
                           child: Column(
                             children: <Widget>[
-                              // sam ? Text('true') : Text('false'),
-                              // kural.kurals[index].isPlaying
-                              //     ? Text('true')
-                              //     : Text('false'),
                               Container(
                                 height: 30,
                                 child: ListView(
@@ -308,27 +310,42 @@ class _HomeState extends State<Home> {
                                 color: Colors.orange,
                               ),
                               ListTile(
-                                trailing: InkWell(
-                                  child: kural.kurals[index].isPlaying == false
-                                      ? Icon(Icons.play_arrow)
-                                      : Platform.isIOS
-                                          ? Icon(Icons.pause)
-                                          : Icon(Icons.stop),
-                                  onTap: () {
-                                    ttsHandler(index);
-                                    setState(() {
-                                      if (kural.kurals[index].isPlaying ==
-                                          false) {
-                                        _speak(kural.kurals[index].kural
-                                            .join(' '));
-                                      } else {
-                                        if (Platform.isIOS) _pause();
-                                        _stop();
-                                      }
-                                      kural.kurals[index].isPlaying =
-                                          !kural.kurals[index].isPlaying;
-                                    });
-                                  },
+                                trailing: Padding(
+                                  padding: const EdgeInsets.only(right: 10.0),
+                                  child: InkWell(
+                                      onDoubleTap: () => _stop,
+                                      child:
+                                          kural.kurals[index].isPlaying == false
+                                              ? Icon(Icons.play_arrow)
+                                              : Platform.isIOS
+                                                  ? Icon(Icons.pause)
+                                                  : Icon(Icons.stop),
+                                      onTap: isTtsPlaying == false ||
+                                              kural.kurals[index].isPlaying ==
+                                                  true
+                                          ? () {
+                                              ttsHandler(index);
+                                              setState(
+                                                () {
+                                                  if (kural.kurals[index]
+                                                          .isPlaying ==
+                                                      false) {
+                                                    _speak(kural
+                                                        .kurals[index].kural
+                                                        .join(' '));
+                                                  } else {
+                                                    if (Platform.isIOS)
+                                                      _pause();
+                                                    _stop();
+                                                  }
+                                                  kural.kurals[index]
+                                                          .isPlaying =
+                                                      !kural.kurals[index]
+                                                          .isPlaying;
+                                                },
+                                              );
+                                            }
+                                          : null),
                                 ),
                                 contentPadding: EdgeInsets.only(
                                   bottom: 20,
@@ -343,7 +360,6 @@ class _HomeState extends State<Home> {
                                   kural.kurals[index].kural[1] +
                                       ' [' +
                                       kural.kurals[index].number.toString() +
-                                      index.toString() +
                                       ']',
                                   style: TextStyle(
                                       fontSize: 14, color: Colors.indigo),
@@ -371,312 +387,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
-// import 'dart:async';
-// import 'dart:io' show Platform;
-// import 'package:flutter/foundation.dart' show kIsWeb;
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_tts/flutter_tts.dart';
-
-// void main() => runApp(Home());
-
-// class Home extends StatefulWidget {
-//   @override
-//   _HomeState createState() => _HomeState();
-// }
-
-// enum TtsState { playing, stopped, paused, continued }
-
-// class _HomeState extends State<Home> {
-//   FlutterTts flutterTts;
-//   dynamic languages;
-//   String language;
-//   double volume = 0.5;
-//   double pitch = 1.0;
-//   double rate = 0.5;
-
-//   String _newVoiceText;
-
-//   TtsState ttsState = TtsState.stopped;
-
-//   var swap = true;
-
-//   get isPlaying => ttsState == TtsState.playing;
-
-//   get isStopped => ttsState == TtsState.stopped;
-
-//   get isPaused => ttsState == TtsState.paused;
-
-//   get isContinued => ttsState == TtsState.continued;
-
-//   @override
-//   initState() {
-//     super.initState();
-//     initTts();
-//   }
-
-//   initTts() {
-//     flutterTts = FlutterTts();
-
-//     _getLanguages();
-
-//     if (!kIsWeb) {
-//       if (Platform.isAndroid) {
-//         _getEngines();
-//       }
-//     }
-
-//     flutterTts.setStartHandler(() {
-//       setState(() {
-//         print("Playing");
-//         ttsState = TtsState.playing;
-//       });
-//     });
-
-//     flutterTts.setCancelHandler(() {
-//       setState(() {
-//         print("Cancel");
-//         ttsState = TtsState.stopped;
-//       });
-//     });
-
-//     if (kIsWeb || Platform.isIOS) {
-//       flutterTts.setPauseHandler(() {
-//         setState(() {
-//           print("Paused");
-//           ttsState = TtsState.paused;
-//         });
-//       });
-
-//       flutterTts.setContinueHandler(() {
-//         setState(() {
-//           print("Continued");
-//           ttsState = TtsState.continued;
-//         });
-//       });
-//     }
-
-//     flutterTts.setErrorHandler((msg) {
-//       setState(() {
-//         print("error: $msg");
-//         ttsState = TtsState.stopped;
-//       });
-//     });
-//   }
-
-//   Future _getLanguages() async {
-//     languages = await flutterTts.getLanguages;
-//     if (languages != null) setState(() => languages);
-//   }
-
-//   Future _getEngines() async {
-//     var engines = await flutterTts.getEngines;
-//     if (engines != null) {
-//       for (dynamic engine in engines) {
-//         print(engine);
-//       }
-//     }
-//   }
-
-//   Future _speak() async {
-//     await flutterTts.setVolume(volume);
-//     await flutterTts.setSpeechRate(rate);
-//     await flutterTts.setPitch(pitch);
-
-//     if (_newVoiceText != null) {
-//       if (_newVoiceText.isNotEmpty) {
-//         var result = await flutterTts.speak(_newVoiceText);
-//         if (result == 1) setState(() => ttsState = TtsState.playing);
-//       }
-//     }
-//   }
-
-//   Future _stop() async {
-//     var result = await flutterTts.stop();
-//     if (result == 1) setState(() => ttsState = TtsState.stopped);
-//   }
-
-//   Future _pause() async {
-//     var result = await flutterTts.pause();
-//     if (result == 1) setState(() => ttsState = TtsState.paused);
-//   }
-
-//   @override
-//   void dispose() {
-//     super.dispose();
-//     flutterTts.stop();
-//   }
-
-//   List<DropdownMenuItem<String>> getLanguageDropDownMenuItems() {
-//     var items = List<DropdownMenuItem<String>>();
-//     for (dynamic type in languages) {
-//       items.add(
-//           DropdownMenuItem(value: type as String, child: Text(type as String)));
-//     }
-//     return items;
-//   }
-
-//   void changedLanguageDropDownItem(String selectedType) {
-//     setState(() {
-//       language = selectedType;
-//       flutterTts.setLanguage(language);
-//     });
-//   }
-
-//   void _onChange(String text) {
-//     setState(() {
-//       _newVoiceText = text;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         home: Scaffold(
-//             appBar: AppBar(
-//               title: Text('Flutter TTS'),
-//             ),
-//             body: ListView.builder(
-//               itemCount: 2,
-//               itemBuilder: (BuildContext context, int index) {
-//                 flutterTts.setCompletionHandler(() {
-//                   setState(() {
-//                     print("Complete");
-//                     swap = !swap;
-//                     ttsState = TtsState.stopped;
-//                   });
-//                 });
-//                 return Column(children: [
-//                   IconButton(
-//                     icon: swap
-//                         ? Icon(Icons.play_arrow)
-//                         : Icon(Icons.pause_circle_outline),
-//                     onPressed: () {
-//                       setState(() {
-//                         swap = !swap;
-//                         _speak();
-//                       });
-//                     },
-//                   ),
-//                   _inputSection(),
-//                   _btnSection(),
-//                   languages != null ? _languageDropDownSection() : Text(""),
-//                   _buildSliders()
-//                 ]);
-//               },
-//             )));
-//   }
-
-//   Widget _inputSection() => Container(
-//       alignment: Alignment.topCenter,
-//       padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
-//       child: TextField(
-//         onChanged: (String value) {
-//           _onChange(value);
-//         },
-//       ));
-
-//   Widget _btnSection() {
-//     if (!kIsWeb && Platform.isAndroid) {
-//       return Container(
-//           padding: EdgeInsets.only(top: 50.0),
-//           child:
-//               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-//             _buildButtonColumn(Colors.green, Colors.greenAccent,
-//                 Icons.play_arrow, 'PLAY', _speak),
-//             _buildButtonColumn(
-//                 Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
-//           ]));
-//     } else {
-//       return Container(
-//           padding: EdgeInsets.only(top: 50.0),
-//           child:
-//               Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-//             _buildButtonColumn(Colors.green, Colors.greenAccent,
-//                 Icons.play_arrow, 'PLAY', _speak),
-//             _buildButtonColumn(
-//                 Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
-//             _buildButtonColumn(
-//                 Colors.blue, Colors.blueAccent, Icons.pause, 'PAUSE', _pause),
-//           ]));
-//     }
-//   }
-
-//   Widget _languageDropDownSection() => Container(
-//       padding: EdgeInsets.only(top: 50.0),
-//       child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-//         DropdownButton(
-//           value: language,
-//           items: getLanguageDropDownMenuItems(),
-//           onChanged: changedLanguageDropDownItem,
-//         )
-//       ]));
-
-//   Column _buildButtonColumn(Color color, Color splashColor, IconData icon,
-//       String label, Function func) {
-//     return Column(
-//         mainAxisSize: MainAxisSize.min,
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           IconButton(
-//               icon: Icon(icon),
-//               color: color,
-//               splashColor: splashColor,
-//               onPressed: () => func()),
-//           Container(
-//               margin: const EdgeInsets.only(top: 8.0),
-//               child: Text(label,
-//                   style: TextStyle(
-//                       fontSize: 12.0,
-//                       fontWeight: FontWeight.w400,
-//                       color: color)))
-//         ]);
-//   }
-
-//   Widget _buildSliders() {
-//     return Column(
-//       children: [_volume(), _pitch(), _rate()],
-//     );
-//   }
-
-//   Widget _volume() {
-//     return Slider(
-//         value: volume,
-//         onChanged: (newVolume) {
-//           setState(() => volume = newVolume);
-//         },
-//         min: 0.0,
-//         max: 1.0,
-//         divisions: 10,
-//         label: "Volume: $volume");
-//   }
-
-//   Widget _pitch() {
-//     return Slider(
-//       value: pitch,
-//       onChanged: (newPitch) {
-//         setState(() => pitch = newPitch);
-//       },
-//       min: 0.5,
-//       max: 2.0,
-//       divisions: 15,
-//       label: "Pitch: $pitch",
-//       activeColor: Colors.red,
-//     );
-//   }
-
-//   Widget _rate() {
-//     return Slider(
-//       value: rate,
-//       onChanged: (newRate) {
-//         setState(() => rate = newRate);
-//       },
-//       min: 0.0,
-//       max: 1.0,
-//       divisions: 10,
-//       label: "Rate: $rate",
-//       activeColor: Colors.green,
-//     );
-//   }
-// }
