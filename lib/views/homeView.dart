e8eb3cc50fccee75fdf59a9b020cf26e55d15716
Sha-Kumar <shashankneeded@gmail.com>
2020-09-23 14:59:46 +0530

    homepage doing

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -11,151 +11,204 @@ class HomeView extends StatelessWidget {
         titleSpacing: 20,
       ),
       body: ListView(padding: const EdgeInsets.all(10.0), children: [
-        InkWell(
-          splashColor: Colors.indigo,
-          hoverColor: Colors.blueAccent,
-          onTap: () {
-            //Todo
-          },
-          child: Row(children: [
-            Container(
-              height: 125,
-              width: 125,
-              child: Icon(
-                Icons.star,
-                color: Colors.red[500],
+        Card(
+          color: Colors.lime,
+          child: InkWell(
+            splashColor: Colors.indigo,
+            onTap: () async {
+              Get.toNamed(Thirukkuralroute);
+            },
+            child: Row(children: [
+              Container(
+                height: 100,
+                width: 120,
+                child: Image.asset('assets/images/thiruvalluvar.png'),
               ),
-            ),
-            Container(
-              color: Colors.orangeAccent,
-              height: 125,
-              width: 500,
-              child: Icon(
-                Icons.star,
-                color: Colors.red[500],
-              ),
-            ),
-          ]),
+              Container(
+                  margin: EdgeInsets.all(8),
+                  height: 100,
+                  width: 225,
+                  child: Column(children: [
+                    Text(
+                      'Thirukkurals',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Open Sans',
+                        fontSize: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Center(
+                      child: Text(
+                        'Thirukkurals of Tamil.Totally 1330 Kurals were there.',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ])),
+            ]),
+          ),
         ),
-        InkWell(
-          splashColor: Colors.indigo,
-         hoverColor: Colors.red,
-          onTap: () async {
-            Get.toNamed(Thirukkuralroute);
-          },
-          child: Row(children: [
-            Container(
-              color: Colors.blueAccent,
-              height: 125,
-              width : 125,
-              child: Card(
+        Card(
+          color: Colors.lime,
+          child: InkWell(
+            splashColor: Colors.indigo,
+            onTap: () async {
+              Get.toNamed(Thirukkuralroute);
+            },
+            child: Row(children: [
+              Container(
+                height: 100,
+                width: 120,
+                child: Image.asset('assets/images/thiruvalluvar.png'),
+                // child: Image(image:  AssetImage('assets/images/avvaiyar.png'),),
+              ),
+              Container(
+                  margin: EdgeInsets.all(8),
+                  height: 100,
+                  width: 225,
+                  child: Column(children: [
+                    Text(
+                      'Thirukkurals',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Open Sans',
+                        fontSize: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Center(
+                      child: Text(
+                        'Aathichudis of Tamil.Totally 109 were there.',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ])),
+            ]),
+          ),
+        ),
+        Card(
+          color: Colors.lime,
+          child: InkWell(
+            splashColor: Colors.indigo,
+            onTap: () async {
+              Get.toNamed(Thirukkuralroute);
+            },
+            child: Row(children: [
+              Container(
+                height: 100,
+                width: 120,
+                child: Image.asset('assets/images/thiruvalluvar.png'),
+              ),
+              Container(
+                  margin: EdgeInsets.all(8),
+                  height: 100,
+                  width: 225,
+                  child: Column(children: [
+                    Text(
+                      'Palamuligaz',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Open Sans',
+                        fontSize: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Center(
+                      child: Text(
+                        'Proverbs of Tamil.Totally 5 proverbs were there.',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ])),
+            ]),
+          ),
+        ),
+        
+        // InkWell(
+        //   splashColor: Colors.indigo,
+        //   hoverColor: Colors.blueAccent,
+        //   onTap: () {
+        //     Get.toNamed(Thirukkuralroute);
+        //   },
+        //   child: Row(children: [
+        //     Container(
+        //       height: 125,
+        //       width: 125,
+        //       child: Icon(
+        //         Icons.star,
+        //         color: Colors.red[500],
+        //       ),
+        //     ),
+        //     Container(
+        //       color: Colors.orangeAccent,
+        //       height: 125,
+        //       width: 500,
+        //       child: Icon(
+        //         Icons.star,
+        //         color: Colors.red[500],
+        //       ),
+        //     ),
+        //   ]),
+        // ),
+        // InkWell(
+        //   splashColor: Colors.indigo,
+        //  hoverColor: Colors.red,
+        //   onTap: () async {
+        //     Get.toNamed(Thirukkuralroute);
+        //   },
+        //   child: Row(children: [
+        //     Container(
+        //       color: Colors.blueAccent,
+        //       height: 125,
+        //       width : 125,
+        //       child: Card(
 
-                child: Text('Aathichudi'),
-              ),
-            ),
-          ]
-          ),
-        ),
-        InkWell(
-          splashColor: Colors.indigo,
-         hoverColor: Colors.red,
-          onTap: () async {
-            Get.toNamed(Thirukkuralroute);
-          },
-          child: Row(children: [
-            Container(
-              height: 125,
-              width : 125,
-              child: Card(
-                child: Text('Proverb'),
-              ),
-            ),
-          ]
-          ),
-        ),
+        //         child: Text('Aathichudi'),
+        //       ),
+        //     ),
+        //   ]
+        //   ),
+        // ),
+        // InkWell(
+        //   splashColor: Colors.indigo,
+        //  hoverColor: Colors.red,
+        //   onTap: () async {
+        //     Get.toNamed(Thirukkuralroute);
+        //   },
+        //   child: Row(children: [
+        //     Container(
+        //       height: 125,
+        //       width : 125,
+        //       child: Card(
+        //         child: Text('Proverb'),
+        //       ),
+        //     ),
+        //   ]
+        //   ),
+        // ),
       ]),
-      // body: ListView(
-      //   padding: const EdgeInsets.all(10.0),
-      //     // shrinkWrap: true,
-      //     children: [
-      //       Material(
-      //         color: Colors.grey[800],
-      //         child: Row(
-      //           children: [
-                  // InkWell(
-                  //   hoverColor: Colors.black,
-                  //     splashColor: Colors.indigo,
-                  //     onTap: () async {
-                  //       Get.toNamed(Thirukkuralroute);
-                  //     },
-                  //     child: Container(
-                  //       height: 125,
-                  //       child: Card(
-                  //         child: Text('Thirukkral'),
-                  //       ),
-                  //     ),
-                  //   ),
-                    // child: Ink.image(
-                    //   image: AssetImage('assets/icon/t1.png'),
-                    //   fit: BoxFit.cover,
-                    //   // width: 300.0,
-                    //   height: 125.0,
-                    //   child: InkWell(
-                    //       onTap: () async{
-                    //         Get.toNamed(Thirukkuralroute);
-                    //       },
-                    //       child: Align(
-                    //         alignment: Alignment.topLeft,
-                    //         child: Padding(
-                    //           padding: const EdgeInsets.all(10.0),
-                    //           child: Text('KITTEN',
-                    //               style: TextStyle(
-                    //                   fontWeight: FontWeight.w900,
-                    //                   color: Colors.white)),
-                    //         ),
-                    //       )),
-                    // ),
-                  
-            //     ],
-            //   ),
-            // // ),
-            // InkWell(
-            //   splashColor: Colors.indigo,
-            //   onTap: () async {
-            //     Get.toNamed(Thirukkuralroute);
-            //   },
-            //   child: Container(
-            //     height: 125,
-            //     child: Card(
-            //       child: Text('Thirukkral'),
-            //     ),
-            //   ),
-            // ),
-            // InkWell(
-          //     splashColor: Colors.indigo,
-          //     onTap: () async {
-          //       Get.toNamed(Thirukkuralroute);
-          //     },
-          //     child: Container(
-          //       height: 125,
-          //       child: Card(
-          //         child: Text('Aathichudi'),
-          //       ),
-          //     ),
-          //   ),
-          //   InkWell(
-          //     splashColor: Colors.indigo,
-          //     onTap: () async {
-          //       Get.toNamed(Thirukkuralroute);
-          //     },
-          //     child: Container(
-          //       height: 125,
-          //       child: Card(
-          //         child: Text('Proverb'),
-          //       ),
-          //     ),
-          //   ),
-          // ]),
+
       // body: Column(
       //   mainAxisAlignment: MainAxisAlignment.center,
 
